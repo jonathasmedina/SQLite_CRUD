c3934312cbe9b8b6d135234eadc9c3ea0f600fdd
Jonathas Leontino Medina <jonathas.medina@ifms.edu.br>
2016-01-21 14:55:49 -0400

    faltando interações entre Activities - parte gráfica

---
+++ app/src/main/java/com/example/asus/myapplication/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.asus.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+
+        //Setar Atributos
+        Button botao = (Button)findViewById(R.id.button);
+
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BancoController crud = new BancoController(getBaseContext());
+                EditText titulo = (EditText)findViewById(R.id.editText);
+                EditText autor = (EditText)findViewById((R.id.editText2));
+                EditText editora = (EditText)findViewById(R.id.editText3);
+                String tituloString = titulo.getText().toString();
+                String autorString = autor.getText().toString();
+                String editoraString = editora.getText().toString();
+                String resultado;
+
+                resultado = crud.insereDado(tituloString,autorString,editoraString);
+
+                Toast.makeText(getApplicationContext(), resultado, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
